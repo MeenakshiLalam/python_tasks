b01324a7123e90089eb487b7e82d532c1eb3caae
MeenakshiLalam <110802650+MeenakshiLalam@users.noreply.github.com>
2022-10-17 18:09:05 +0530

    Update calculate.py

--- calculate.py
+++ calculate.py
@@ -46,4 +46,34 @@ while next_calculation=="yes":
         if next_calculation != "yes":
             break
 
-print(f"{count} calculations users did: ")
+print(f"{count} calculations users did ")
+
+
+
+OUTPUT:
+     
+list of operations
+1.Add
+2.Subtract
+3.Multiply
+4.Divide
+Enter first number: 22
+Enter second number: 33
+22 + 33 = 55
+22 - 33 = -11
+22 * 33 = 726
+22 / 33 = 0.6666666666666666
+If you want to do next calculation? (yes/no): yes
+Enter first number: 88
+Enter second number: 0
+88 + 0 = 88
+88 - 0 = 88
+88 * 0 = 0
+zero division error
+88 / 0 = None
+If you want to do next calculation? (yes/no): yes
+Enter first number: uu
+Enter second number: ll
+invalid number
+If you want to do next calculation? (yes/no): no
+3 calculations users did
