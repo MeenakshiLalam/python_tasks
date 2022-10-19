83f4566f587f6fc75d0defea9723263b7568b4f9
MeenakshiLalam <110802650+MeenakshiLalam@users.noreply.github.com>
2022-10-19 23:25:26 +0530

    Update Git_Api.py

--- Git_Api.py
+++ Git_Api.py
@@ -1,10 +1,14 @@
-
 import requests
-
+response = requests.get("https://api.github.com/users/MeenakshiLalam/repos")
 username="MeenakshiLalam"
 url=f"https://api.github.com/users/{username}"
-projects=requests.get(url).json()
-print(projects)
+my_projects = response.json()
+print("list of Github urls")
+for projects in my_projects:
+    print( projects['url'])
+
+print("list of repository names")
+
 import base64
 from github import Github
 username="MeenakshiLalam"
@@ -13,11 +17,24 @@ user=g.get_user(username)
 for repo in user.get_repos():
     print(repo)
 
+
+ 
+
+
     
     
     
  OUTPUT:
-  Repository(full_name="MeenakshiLalam/eks-demos")
+    
+list of Github urls
+https://api.github.com/repos/MeenakshiLalam/eks-demos
+https://api.github.com/repos/MeenakshiLalam/hello_from_git
+https://api.github.com/repos/MeenakshiLalam/mohan
+https://api.github.com/repos/MeenakshiLalam/python-assignments
+https://api.github.com/repos/MeenakshiLalam/python_tasks
+
+list of repository names
+Repository(full_name="MeenakshiLalam/eks-demos")
 Repository(full_name="MeenakshiLalam/hello_from_git")
 Repository(full_name="MeenakshiLalam/mohan")
 Repository(full_name="MeenakshiLalam/python-assignments")
